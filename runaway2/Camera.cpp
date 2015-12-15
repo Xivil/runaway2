8291f17979d0a7003324767ec648ad9d50971f44
kohei <kou03htm@gmail.com>
2015-12-16 02:24:16 +0900

    a

--- runaway2/Camera.cpp
+++ runaway2/Camera.cpp
@@ -1,5 +1,6 @@
 #include "Camera.h"
 #include "Func.h"
+#include "Quaternion.h"
 Camera::Camera(){
 	position = km::GetVector3(0, 3, 0);
 	target = km::GetVector3(0, 3, 5);
@@ -18,18 +19,23 @@ void Camera::SetTarget(km::Vector3& a){
 }
 
 void Camera::UseCamera(){
+	static float angley = 0;
 	position += vector;
 	direction.Normalize();
-	unit = direction;
-	target = position + unit;
+	unit = target - position;
+	unit.Normalize();
+	//target = position + unit;
+	target = km::AnyAxis(target, position, 1,0, 1, 0 );
 	SetCameraPositionAndTarget_UpVecY(Vector3ToDxVector(position), Vector3ToDxVector(target));
+	//SetCameraPositionAndAngle(Vector3ToDxVector(position), 0, DEG_TO_RAD(angley), 0);
+	
 	if (Keyboard_Get(KEY_INPUT_LEFT)){
 		
-		direction += km::GetVector3(-0.1, 0, -0.1);
+		//direction += km::GetVector3(-0.1, 0, -0.1);
 		
 	}
 	if (Keyboard_Get(KEY_INPUT_RIGHT)){
-		direction += km::GetVector3(0.1, 0, 0.1);
+		//direction += km::GetVector3(0.1, 0, 0.1);
 	}
 	if (Keyboard_Get(KEY_INPUT_UP)){
 		vector = unit;
@@ -37,6 +43,6 @@ void Camera::UseCamera(){
 	else{
 		vector *= 0;
 	}
-	DrawFormatString(0, 0, GetColor(255,255,255), "%f, %f, %f", unit.x, unit.y, unit.z);
-	DrawFormatString(0, 30, GetColor(255, 255, 255), "%f, %f, %f", direction.x, unit.y, direction.z);
+	DrawFormatString(0, 0, GetColor(255,255,255), "%f, %f, %f", angley, unit.y, unit.z);
+	DrawFormatString(0, 30, GetColor(255, 255, 255), "%f, %f, %f", unit.x, unit.y, unit.z);
 }
